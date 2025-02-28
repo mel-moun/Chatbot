00a9d0175869f1e7792a96f034869406a5e82d2e
mel-moun <mrymgithub@gmail.com>
2025-02-28 15:12:10 +0000

    views

--- Backend/users/views.py
+++ Backend/users/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate
+from rest_framework import status, permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
+from .serializers import RegisterSerializer, LoginSerializer
 
-# Create your views here.
+from rest_framework import status, permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
+from .serializers import RegisterSerializer
