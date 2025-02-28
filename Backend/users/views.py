b633c9fe00da7e07e7e6c3e23c5ea38fc26b4350
mel-moun <mrymgithub@gmail.com>
2025-02-28 15:12:38 +0000

    viieew

--- Backend/users/views.py
+++ Backend/users/views.py
@@ -10,3 +10,21 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
 from .serializers import RegisterSerializer
+
+class RegisterAPIView(APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request):
+        print("Request Data:", request.data)
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            refresh = RefreshToken.for_user(user)
+            access_token = refresh.access_token
+            return Response({
+                'access': str(access_token),
+                'refresh': str(refresh)
+            }, status=status.HTTP_201_CREATED)
+        else:
+            print("Validation Errors:", serializer.errors)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
