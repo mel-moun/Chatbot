89d5a181ec1c5711fea3f1fe22c8e8ba5e814d08
mel-moun <mrymgithub@gmail.com>
2025-02-28 15:31:05 +0000

    js

--- Frontend/js/main.js
+++ Frontend/js/main.js
@@ -38,6 +38,10 @@ document.addEventListener('DOMContentLoaded', () => {
             : 'http://127.0.0.1:8000/api/users/register/';
 
         fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
             body: JSON.stringify(data)
         })
         .then(response => response.json())
