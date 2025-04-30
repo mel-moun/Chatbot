4a34d079cc25cd19b07c601431baf5612d5a5c10
HoudaChairi <hchairi1902@gmail.com>
2025-04-30 14:45:10 +0100

    some security added

--- Backend/backend/settings.py
+++ Backend/backend/settings.py
@@ -19,11 +19,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d4_mraovhzb318zx6*qf+o$k^i*=_l84*6j*^yvc9$vab2f^0y'
+from decouple import config
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
